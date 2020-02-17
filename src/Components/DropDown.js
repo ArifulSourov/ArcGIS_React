fdfe9f06391cfb1c556ab19aecad0b555d213339
Md.Ariful Islam Sourov <35974143+ArifulSourov@users.noreply.github.com>
2020-02-17 18:28:02 +0600

    Update DropDown.js

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,27 +1,79 @@
-import React, {
-    Component
-} from 'react';
-import * as ReactBootStrap from 'react-bootstrap'
+import React, { Component } from 'react';
+//import Axios from 'axios';
+// import * as ReactBootStrap from 'react-bootstrap'
 
 class DropDown extends Component {
-    constructor() {
-        super()
-        this.state = true;
+    constructor(props){
+        super(props)
+        this.state = {
+            item: [],
+            isLoaded: true,
+            district: [],
+        }
+        // let [items] = this.setState
+        // console.log(items.drawingInfo.renderer.uniqueValueInfos.value)
     }
 
+    componentDidMount() {
+        fetch('http://103.58.93.172/tillerwebadaptor/rest/services/DPHE/Bangladesh/MapServer/0?f=pjson')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    isLoaded: false,
+                    item: responseJson.drawingInfo.renderer.uniqueValueInfos,
+                    district: responseJson.fields,
+                    
+                })
+                
+                
+            })
+            .catch((error) =>{
+                console.log(error)
+            })
+            
+    }
+    handleChange = (itemD) => {
+        const value = itemD.target.value;
+        this.setState({
+            value: value
+            
+        });
+        
+        console.log(value)
+        
+    };
+    
+    
     render() {
-        return ( 
-        <div >
-            <ReactBootStrap.DropdownButton variant = 'transparent' id = "dropdown-button" title = "Dropdown button" >
-                <ReactBootStrap.Dropdown.Item> Action </ReactBootStrap.Dropdown.Item>  
-                <ReactBootStrap.Dropdown.Divider / >
-                <ReactBootStrap.Dropdown.Item > Another action </ReactBootStrap.Dropdown.Item> 
-                <ReactBootStrap.Dropdown.Divider / >
-                <ReactBootStrap.Dropdown.Item > Something else </ReactBootStrap.Dropdown.Item> 
-                </ReactBootStrap.DropdownButton>
+        const {item} = this.state;
+        const itemDropdown = item.map((division, index)=> (
+            <option key={index}>{division.value}</option>
+      
+        ))
+        const {district} = this.state;
+        const dist = district.map((districts, i)=> (
+        <option key={i}>{districts.alias}</option>
 
+        )) 
+    
+        //console.log(dist)
+        //console.log(itemDropdown)
+        return (
+
+            <div>
+                <div>
+                    <select onChange = {this.handleChange}>
+                        {itemDropdown}
+                    </select>
+              
+                    <select onChange = {this.handleChange}> 
+                        {dist} 
+                    </select> 
                 </div>
+            </div>
+            
         );
+        
     }
 }
 
